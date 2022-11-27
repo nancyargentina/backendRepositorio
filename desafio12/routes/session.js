499ec294e7efcf46452bdebe24396b0b84bbabc3
Nancy Carolina Martinez <zoe.arg@gmail.com>
2022-11-27 20:58:04 -0300

    cierre de session

--- desafio12/routes/session.js
+++ desafio12/routes/session.js
@@ -6,7 +6,7 @@ let router = new Router();
 router.get("/login", (req, res) => {
     const usuario = req.session.nombre;
     if (usuario) {
-        res.render("insertProduct",{data:req.session.nombre});
+        res.render("insertProduct", { data: req.session.nombre });
     } else {
         res.render("login");
     }
@@ -18,7 +18,17 @@ router.post("/login", (req, res) => {
     for (const key in req.body) {
         req.session[key] = req.body[key];
     }
-    res.redirect('/login');
+    res.redirect("/login");
 });
 
+router.get("/logout", (req, res) => {
+    const nombre = req.session.nombre;
+    req.session.destroy((error) => {
+        if (error) {
+            res.send("Error cerrando la sesión");
+        } else {
+            res.render("logout", { data: nombre });
+        }
+    });
+});
 module.exports = router;
